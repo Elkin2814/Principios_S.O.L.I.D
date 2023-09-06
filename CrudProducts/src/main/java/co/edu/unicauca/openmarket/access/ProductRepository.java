359e78d8cc17eae60988064c3560e3cf7fac290c
Elkin2814 <elkingmorillo@gmail.com>
2023-09-06 18:48:55 -0500

    Nueva clase e interfaz

---
+++ CrudProducts/src/main/java/co/edu/unicauca/openmarket/access/ProductRepository.java
@@ -0,0 +1,160 @@
+package co.edu.unicauca.openmarket.access;
+
+import co.edu.unicauca.openmarket.domain.Product;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Es una implementación que tiene libertad de hacer una implementación del
+ * contrato. Lo puede hacer con Sqlite, postgres, mysql, u otra tecnología
+ *
+ * @author Libardo, Julio
+ */
+public class ProductRepository implements IProductRepository {
+
+    private AssistentDB conn = new AssistentDB();
+    
+
+    public ProductRepository() {
+        conn.initDatabase();
+    }
+
+    @Override
+    public boolean save(Product newProduct) {
+
+        try {
+            //Validate product
+            if (newProduct == null || newProduct.getName().isBlank()) {
+                return false;
+            }
+            //this.connect();
+
+            String sql = "INSERT INTO products ( name, description ) "
+                    + "VALUES ( ?, ? )";
+
+            PreparedStatement pstmt = conn.getConn().prepareStatement(sql);
+            pstmt.setString(1, newProduct.getName());
+            pstmt.setString(2, newProduct.getDescription());
+            pstmt.executeUpdate();
+            //this.disconnect();
+            return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(ProductRepository.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }
+
+    @Override
+    public List<Product> findAll() {
+        List<Product> products = new ArrayList<>();
+        try {
+
+            String sql = "SELECT * FROM products";
+            //this.connect();
+
+            Statement stmt = conn.getConn().createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            while (rs.next()) {
+                Product newProduct = new Product();
+                newProduct.setProductId(rs.getLong("productId"));
+                newProduct.setName(rs.getString("name"));
+                newProduct.setDescription(rs.getString("description"));
+
+                products.add(newProduct);
+            }
+            //this.disconnect();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(ProductRepository.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return products;
+    }
+
+    
+    @Override
+    public boolean edit(Long id, Product product) {
+        try {
+            //Validate product
+            if (id <= 0 || product == null) {
+                return false;
+            }
+            //this.connect();
+
+            String sql = "UPDATE  products "
+                    + "SET name=?, description=? "
+                    + "WHERE productId = ?";
+
+            PreparedStatement pstmt = conn.getConn().prepareStatement(sql);
+            pstmt.setString(1, product.getName());
+            pstmt.setString(2, product.getDescription());
+            pstmt.setLong(3, id);
+            pstmt.executeUpdate();
+            //this.disconnect();
+            return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(ProductRepository.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        try {
+            //Validate product
+            if (id <= 0) {
+                return false;
+            }
+            //this.connect();
+
+            String sql = "DELETE FROM products "
+                    + "WHERE productId = ?";
+
+            PreparedStatement pstmt = conn.getConn().prepareStatement(sql);
+            pstmt.setLong(1, id);
+            pstmt.executeUpdate();
+            //this.disconnect();
+            return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(ProductRepository.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }
+
+    @Override
+    public Product findById(Long id) {
+        try {
+
+            String sql = "SELECT * FROM products  "
+                    + "WHERE productId = ?";
+
+            PreparedStatement pstmt = conn.getConn().prepareStatement(sql);
+            pstmt.setLong(1, id);
+
+            ResultSet res = pstmt.executeQuery();
+
+            if (res.next()) {
+                Product prod = new Product();
+                prod.setProductId(res.getLong("productId"));
+                prod.setName(res.getString("name"));
+                prod.setDescription(res.getString("description"));
+                return prod;
+            } else {
+                return null;
+            }
+            //this.disconnect();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(ProductRepository.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+}
